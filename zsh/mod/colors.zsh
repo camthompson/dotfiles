6627a51c77616eaac565cbe7bb15c77215b5e8d5
Cam Thompson <cam@camthompson.com>
2013-05-16 14:01:43 -0400

    Use black on blue for less standout

--- zsh/mod/colors.zsh
+++ zsh/mod/colors.zsh
@@ -5,7 +5,7 @@ export LESS_TERMCAP_mb=$'\E[01;31m'      # Begins blinking.
 export LESS_TERMCAP_md=$'\E[0;36m'       # Begins bold.
 export LESS_TERMCAP_me=$'\E[0m'          # Ends mode.
 export LESS_TERMCAP_se=$'\E[0m'          # Ends standout-mode.
-export LESS_TERMCAP_so=$'\E[0;37;104m'    # Begins standout-mode.
+export LESS_TERMCAP_so=$'\E[0;30;44m'    # Begins standout-mode.
 export LESS_TERMCAP_ue=$'\E[0m'          # Ends underline.
 export LESS_TERMCAP_us=$'\E[0;32m'       # Begins underline.
 
