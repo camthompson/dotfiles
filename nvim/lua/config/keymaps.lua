123ab71fe013610df1c7f0e3fa08332c391c08dc
Cam Thompson <cam@camthompson.com>
2025-09-01 09:15:42 -0400

    ; and <cr> maps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Bind ; to : in normal mode
+vim.keymap.set("n", ";", ":", { desc = "Command mode" })
+
+-- Bind <cr> to :up<cr> in normal mode
+vim.keymap.set("n", "<cr>", ":up<cr>", { desc = ":up" })
