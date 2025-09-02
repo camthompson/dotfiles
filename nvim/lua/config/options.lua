7650ad46973c234005d5415f05b2e3c66192aff9
Cam Thompson <cam@camthompson.com>
2025-09-02 09:46:13 -0400

    hide cursor on dashboard

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -6,3 +6,24 @@ local opt = vim.opt
 
 opt.clipboard = "" -- Don't integrate with system clipboard
 opt.relativenumber = false -- Show absolute line numbers
+
+vim.api.nvim_create_autocmd("User", {
+  -- Hide cursor on dashboard
+  pattern = "SnacksDashboardOpened",
+  callback = function()
+    local hl = vim.api.nvim_get_hl(0, { name = "Cursor", create = true })
+    hl.blend = 100
+    vim.api.nvim_set_hl(0, "Cursor", hl)
+    vim.cmd("set guicursor+=a:Cursor/lCursor")
+  end,
+})
+vim.api.nvim_create_autocmd("User", {
+  pattern = "SnacksDashboardClosed",
+  callback = function()
+    local hl = vim.api.nvim_get_hl(0, { name = "Cursor", create = true })
+    hl.blend = 0
+    vim.api.nvim_set_hl(0, "Cursor", hl)
+    -- vim.opt.guicursor.append("a:Cursor/lCursor")
+    vim.cmd("set guicursor+=a:Cursor/lCursor")
+  end,
+})
