c2842b61b1d759f275e375cd0ff777a269ac32d3
Cam Thompson <cam@camthompson.com>
2025-09-20 16:10:12 -0400

    install blink-emoji

---
+++ nvim/lua/plugins/blink-emoji.lua
@@ -0,0 +1,35 @@
+return {
+  "saghen/blink.cmp",
+  dependencies = {
+    "moyiz/blink-emoji.nvim",
+  },
+  opts = {
+    sources = {
+      default = {
+        "emoji",
+      },
+      providers = {
+        emoji = {
+          module = "blink-emoji",
+          name = "Emoji",
+          score_offset = 15, -- Tune by preference
+          opts = {
+            insert = true, -- Insert emoji (default) or complete its name
+            ---@type string|table|fun():table
+            trigger = function()
+              return { ":" }
+            end,
+          },
+          should_show_items = function()
+            return vim.tbl_contains(
+              -- Enable emoji completion only for git commits and markdown.
+              -- By default, enabled for all file-types.
+              { "gitcommit", "markdown" },
+              vim.o.filetype
+            )
+          end,
+        },
+      },
+    },
+  },
+}
