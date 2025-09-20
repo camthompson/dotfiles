c902ff66fd7582cca0556e907887407c822be322
Cam Thompson <cam@camthompson.com>
2025-09-20 17:22:03 -0400

    bring back lualine separators

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -3,8 +3,5 @@ return {
   "nvim-lualine/lualine.nvim",
   opts = function(_, opts)
     table.remove(opts.sections.lualine_z)
-    opts.options = opts.options or {}
-    opts.options.section_separators = { left = "", right = "" }
-    opts.options.component_separators = { left = ":", right = "" }
   end,
 }
