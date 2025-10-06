76158a2784b29defbb20685e783718b41cf81aef
Cam Thompson <cam@camthompson.com>
2025-10-06 14:40:41 -0400

    lock leap to working version

---
+++ nvim/lua/plugins/leap.lua
@@ -0,0 +1,5 @@
+return {
+  "ggandor/leap.nvim",
+  -- Pin to commit before the breaking change that converted safe_labels from table to string
+  commit = "07304103f6bd923004fdef9262d9a4d7925fb70a",
+}
