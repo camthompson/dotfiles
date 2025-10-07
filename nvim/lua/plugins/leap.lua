16899898cf891096b2d4946fe103d0952e9367d2
Cam Thompson <cam@camthompson.com>
2025-10-07 15:50:59 -0400

    use my fork of flit for now, update other packages

--- nvim/lua/plugins/leap.lua
+++ nvim/lua/plugins/leap.lua
@@ -1,6 +1,6 @@
 return {
-  "ggandor/leap.nvim",
-  -- Pin to commit before the breaking change that converted safe_labels from table to string
-  -- https://github.com/ggandor/flit.nvim/issues/54
-  commit = "a755cea5ec06191b46702ac8fde8ef03ad2fbdeb",
+  {
+    "camthompson/flit.nvim",
+    branch = "fix/leap-string-labels-compat",
+  },
 }
