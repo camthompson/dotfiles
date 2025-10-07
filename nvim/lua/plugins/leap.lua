6085e356a8ff381866eff748e71d54a237c4e718
Cam Thompson <cam@camthompson.com>
2025-10-07 10:08:24 -0400

    use slightly newer commit, link to pr

--- nvim/lua/plugins/leap.lua
+++ nvim/lua/plugins/leap.lua
@@ -1,5 +1,6 @@
 return {
   "ggandor/leap.nvim",
   -- Pin to commit before the breaking change that converted safe_labels from table to string
-  commit = "07304103f6bd923004fdef9262d9a4d7925fb70a",
+  -- https://github.com/ggandor/flit.nvim/issues/54
+  commit = "a755cea5ec06191b46702ac8fde8ef03ad2fbdeb",
 }
