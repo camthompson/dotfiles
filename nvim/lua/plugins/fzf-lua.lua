c3ac668faed4efcc584e3a13636b237f4e8c0948
Cam Thompson <cam@camthompson.com>
2025-11-06 12:09:27 -0500

    scope oldfiles to cwd

--- nvim/lua/plugins/fzf-lua.lua
+++ nvim/lua/plugins/fzf-lua.lua
@@ -3,7 +3,7 @@ return {
   keys = {
     { "<leader><leader>", "<cmd>FzfLua git_files<cr>", desc = "Find Files (git-files)" },
     { "<leader>fa", LazyVim.pick("files"), desc = "Find Files (Root Dir)" },
-    { ",,", "<cmd>FzfLua oldfiles<cr>", desc = "Recent" },
+    { ",,", LazyVim.pick("oldfiles", { cwd = vim.uv.cwd() }), desc = "Recent (cwd)" },
     { "<leader>;", "<cmd>FzfLua command_history<cr>", desc = "Command History" },
   },
 }
