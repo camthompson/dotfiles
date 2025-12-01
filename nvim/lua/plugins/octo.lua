f95667c41a0b341af86f332a2778d3f73970df83
Cam Thompson <cam@camthompson.com>
2025-12-01 12:37:52 -0500

    replace octo with snacks

--- nvim/lua/plugins/octo.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "pwntester/octo.nvim",
-  opts = {
-    suppress_missing_scope = {
-      projects_v2 = true,
-    },
-  },
-}
--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -5,4 +5,34 @@ return {
       enabled = false,
     },
   },
+  keys = {
+    {
+      "<leader>gi",
+      function()
+        Snacks.picker.gh_issue()
+      end,
+      desc = "GitHub Issues (open)",
+    },
+    {
+      "<leader>gI",
+      function()
+        Snacks.picker.gh_issue({ state = "all" })
+      end,
+      desc = "GitHub Issues (all)",
+    },
+    {
+      "<leader>gp",
+      function()
+        Snacks.picker.gh_pr()
+      end,
+      desc = "GitHub Pull Requests (open)",
+    },
+    {
+      "<leader>gP",
+      function()
+        Snacks.picker.gh_pr({ state = "all" })
+      end,
+      desc = "GitHub Pull Requests (all)",
+    },
+  },
 }
