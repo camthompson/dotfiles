03a9938e5be4d53e30d88ed9ba8ee3ece564da66
Cam Thompson <cam@camthompson.com>
2026-01-18 01:56:48 -0500

    install oil.nvim, update

---
+++ nvim/lua/plugins/oil.lua
@@ -0,0 +1,9 @@
+return {
+  "stevearc/oil.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {},
+  cmd = "Oil",
+  keys = {
+    { "-", "<cmd>Oil<cr>", desc = "Open parent directory" },
+  },
+}
