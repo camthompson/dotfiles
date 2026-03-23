78876b1c1f3ec7d6f1b073decac58b1206d176d3
Cam Thompson <cam@camthompson.com>
2026-03-23 11:52:05 -0400

    update nvim plugins

--- nvim/lua/plugins/deltaview.lua
+++ nvim/lua/plugins/deltaview.lua
@@ -1,5 +1,6 @@
 return {
   "kokusenz/deltaview.nvim",
+  dependencies = { "kokusenz/delta.lua" },
   cmd = { "DeltaView", "DeltaMenu", "Delta" },
   keys = {
     { "<leader>gv", "<cmd>DeltaView<cr>", desc = "DeltaView (file)" },
