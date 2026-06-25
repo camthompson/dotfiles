e252aacca8442301216e5a6203845abf8a0b7f2e
Cam Thompson <cam@camthompson.com>
2026-06-25 10:56:01 -0400

    replace deprecated yank option

--- nvim/lua/core/autocmds.lua
+++ nvim/lua/core/autocmds.lua
@@ -16,7 +16,7 @@ vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
 vim.api.nvim_create_autocmd("TextYankPost", {
   group = augroup("highlight_yank"),
   callback = function()
-    (vim.hl or vim.highlight).on_yank()
+    vim.hl.hl_op()
   end,
 })
 
